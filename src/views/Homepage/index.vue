27c0ee10d9b5fd64f4e5a1b5dfa530ea5935e24f
dxh98 <787805458@qq.com>
2020-01-10 22:15:11 +0800

    爆笑段子页面简单的数据展示和页面结构

--- src/views/Homepage/index.vue
+++ src/views/Homepage/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <van-tabs line-width="0">
+    <van-tabs ref="toubu" line-width="0" >
       <van-tab title="爆笑段子" :to="{name:'funtext'}"></van-tab>
       <van-tab title="搞笑图片" :to="{name:'funpicture'}"></van-tab>
       <van-tab title="滑稽动图" :to="{name:'fungif'}"></van-tab>
@@ -8,6 +8,8 @@
     <keep-alive>
       <router-view />
     </keep-alive>
+
+    
   </div>
 </template>
 
@@ -18,9 +20,15 @@ export default {
     return {
       
     };
-  }
+   
+  },
+  created() {
+
+  },
+  
 };
 </script>
 
 <style scoped>
+
 </style>
